4303bf255720c3d2c597a83428b59d7a96600f3f
Sameer <liteshgoyal55@gmail.com>
2024-08-26 09:44:11 +0530

    initial commit

---
+++ First_tutorial/Demo.java
@@ -0,0 +1,7 @@
+package First_tutorial;
+
+public class Demo {
+    public static void main(String[] args){
+        System.out.println(args[1]);
+    }
+}
---
+++ First_tutorial/Main.java
@@ -0,0 +1,6 @@
+package First_tutorial;
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
